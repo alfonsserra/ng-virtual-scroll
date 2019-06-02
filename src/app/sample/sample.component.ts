20de547c1aa1f0bdd78e0b413be5c8e6f24570ce
alfonsserra <alfonsoserra@gmail.com>
2019-06-02 19:59:54 +0200

    Update to Angular 8

--- src/app/sample/sample.component.ts
+++ src/app/sample/sample.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector:    'app-sample',
   templateUrl: './sample.component.html',
   styleUrls:   ['./sample.component.scss']
 })
-export class SampleComponent implements OnInit {
+export class SampleComponent implements OnInit, OnDestroy {
 
   @Input() public n = 1;
 
@@ -13,6 +13,10 @@ export class SampleComponent implements OnInit {
   }
 
   ngOnInit() {
+    console.log('On init ' + this.n);
   }
 
+  ngOnDestroy() {
+    console.log('On destroy ' + this.n);
+  }
 }
